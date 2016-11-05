09c746f7991ea0320691175dde8c597bfaaf44ba
jacopo1395 <jack1395@hotmail.it>
2016-11-05 17:52:18 +0100

    initial commit

---
+++ app/src/main/java/com/triviamusic/triviamusicandroid/MainActivity.java
@@ -0,0 +1,13 @@
+package com.triviamusic.triviamusicandroid;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
