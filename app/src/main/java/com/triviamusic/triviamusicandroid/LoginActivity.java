34c21ba6bf5c7d0661fee4dd842ec171ce0c758d
jacopo1395 <jack1395@hotmail.it>
2017-01-21 17:15:58 +0100

    signup

--- app/src/main/java/com/triviamusic/triviamusicandroid/LoginActivity.java
+++ app/src/main/java/com/triviamusic/triviamusicandroid/LoginActivity.java
@@ -1,11 +1,16 @@
 package com.triviamusic.triviamusicandroid;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import com.google.firebase.auth.FirebaseAuth;
@@ -19,11 +24,13 @@ public class LoginActivity extends AppCompatActivity {
     private static final String TAG = "TAG";
     private FirebaseAuth mAuth;
     private FirebaseAuth.AuthStateListener mAuthListener;
+    private Context context;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        this.context=getApplicationContext();
         TextView signup = (TextView) findViewById(R.id.mainSignup);
         signup.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -33,34 +40,50 @@ public class LoginActivity extends AppCompatActivity {
                 finish();
             }
         });
-//        mAuth = FirebaseAuth.getInstance();
-//        mAuthListener = new FirebaseAuth.AuthStateListener() {
-//            @Override
-//            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
-//                FirebaseUser user = firebaseAuth.getCurrentUser();
-//                if (user != null) {
-//                    // User is signed in
-//                    Log.d(TAG, "onAuthStateChanged:signed_in:" + user.getUid());
-//                } else {
-//                    // User is signed out
-//                    Log.d(TAG, "onAuthStateChanged:signed_out");
-//                }
-//                // ...
-//            }
-//        };
+        mAuth = FirebaseAuth.getInstance();
+        Button login = (Button) findViewById(R.id.mainLogin);
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                EditText email = (EditText) findViewById(R.id.mainUsername);
+                EditText password = (EditText) findViewById(R.id.mainPassword);
+                mAuth.signInWithEmailAndPassword(email.getText().toString(), password.getText().toString());
+
+            }
+        });
+        mAuthListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+                if (user != null) {
+                    // User is signed in
+                    Log.d(TAG, "onAuthStateChanged:signed_in:" + user.getUid());
+                    Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                    startActivity(intent);
+                    finish();
+                } else {
+                    // User is signed out
+                    Log.d(TAG, "onAuthStateChanged:signed_out");
+                    Snackbar snackbar = Snackbar
+                            .make(findViewById(android.R.id.content), "Welcome to AndroidHive", Snackbar.LENGTH_LONG);
+                    snackbar.show();
+                }
+                // ...
+            }
+        };
     }
 
-//    @Override
-//    public void onStart() {
-//        super.onStart();
-//        mAuth.addAuthStateListener(mAuthListener);
-//    }
-//
-//    @Override
-//    public void onStop() {
-//        super.onStop();
-//        if (mAuthListener != null) {
-//            mAuth.removeAuthStateListener(mAuthListener);
-//        }
-//    }
+    @Override
+    public void onStart() {
+        super.onStart();
+        mAuth.addAuthStateListener(mAuthListener);
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        if (mAuthListener != null) {
+            mAuth.removeAuthStateListener(mAuthListener);
+        }
+    }
 }
